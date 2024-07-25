cf0b9b92f935fab86df5d00487f70c2fa57e9aac
Owen <x11161125@gmail.com>
2024-07-25 16:24:47 +0800

    using Data Annotations to Validate

--- RazorPagesBooks/Models/Book.cs
+++ RazorPagesBooks/Models/Book.cs
@@ -5,15 +5,25 @@ namespace RazorPagesBooks.Models
     public class Book
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
         public string ProductNo { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
         public string ProductName { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string Type { get; set; }
+        [Required]
+        [Range(1, 1000000)]
+        [DataType(DataType.Currency)]
         public int Price { get; set; }
 
         [Display(Name = "Publication Date")]
         [DataType(DataType.Date)]
         public DateTime PublicationDate { get; set; }
-
+        [Range(1, 100)]
         public string Rating { get; set; } = string.Empty;
     }
 }
